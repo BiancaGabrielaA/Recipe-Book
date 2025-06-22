09d721461ec77a1bc9b011b36b5c611e223f16b0
BiancaGabrielaA <biancagabriela.asavoaei@gmail.com>
2025-06-22 21:59:59 +0300

    Delete src directory

--- src/app/recipe-type.ts
+++
@@ -1,9 +0,0 @@
-export interface RecipeType {
-    id: number;
-    name: string;
-    imageURL: string;
-    description: string;
-    ingredients: string[];
-    steps: string[];
-    location: string;
-}
