ae198f3cb7b8453b2413ba28efda498b9fc3a4cc
BiancaGabrielaA <biancagabriela.asavoaei@gmail.com>
2025-06-22 13:46:13 +0300

    HOME

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 import { CommonModule } from '@angular/common';
 import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, CommonModule],
+  imports: [RouterOutlet, NavbarComponent, CommonModule, FooterComponent],
   template: `
       <main class="main">
         <div class="content">
             <div *ngIf="sessionToken">
                 <app-navbar ></app-navbar>
+                <app-footer></app-footer>
             </div>
             <router-outlet />
         </div>
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,9 +5,29 @@ import { Component } from '@angular/core';
   imports: [],
   standalone: true,
   template: `
-    <p>
-      home works!
-    </p>
+    <main class="home">
+        <section class="home__intro">
+            <div class="home__intro__text"> 
+                <span>Welcome to our international kitcken</span>
+                <p>Your Kitchen. Our Recipes. Endless Possibilities.</p>
+                <button>Start Cooking – Find or Add Recipes <i>&rarr;</i></button>
+            </div>
+            <div class="home__intro__img">
+                <img src="/assets/home-img.jpg" alt="Home Image" aria-hidden="true" />
+            </div>
+        </section>
+        <section class="home__details">
+           <div class="home__details__graphic">
+             
+           </div>
+           <div class="home__details__number">
+              +5000 users
+           </div>
+           <div class="home__details__geographic">
+             +15 countries
+           </div>
+        </section>
+    </main>
   `,
   styleUrl: './home.component.scss'
 })
